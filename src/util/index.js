893f322be2ec6f57a1f3d446a9870e1b4135b503
b1018043 <grey.fox.0417@gmail.com>
2020-03-02 20:28:55 +0900

    feat曜日の計算をする関数の実装

---
+++ src/util/index.js
@@ -0,0 +1,11 @@
+
+// 月 2 火 3 水 4 木 5 金 6 土 0 日 1 
+export const calcDayOfWeek=(raw_y,raw_m,d)=>{
+    const { floor }= Math;
+    const m=(raw_m<=2)?raw_m+12:raw_m;
+    const y = (raw_m <= 2) ? raw_y-1 : raw_y;
+    const C=floor(y/100);
+    const L=5*C+floor(C/4);
+    const Y=y%100;
+    return (d+floor(26*(m+1)/10)+Y+floor(Y/4)+L)%7;
+}
